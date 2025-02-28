6ade1a3f449657cce1e770b973631c87a668d7b6
Anureet Kaur <anureet0851.becse24@chitkara.edu.in>
2025-02-28 14:38:42 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,8 +17,14 @@ int main(){
         printf("%d",results);
     }
     else if(c = '/'){
+        if(a%b==0){
         result = a/b;
         printf("%d",result);
+        }
+        else{
+            printf("error");
+        }
+
     }            
     return 0;
 }
